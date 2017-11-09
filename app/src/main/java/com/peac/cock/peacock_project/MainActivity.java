00ebcad7e499e7af7e0493dd6b2f976b36b47296
dahye <dahye96@gmail.com>
2017-11-10 00:05:45 +0900

    온갖 방법을 다 써도 파이차트 안되네 왜 그런지 아는 사람 손 좀 들어봐

--- app/src/main/java/com/peac/cock/peacock_project/MainActivity.java
+++ app/src/main/java/com/peac/cock/peacock_project/MainActivity.java
@@ -118,7 +118,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+       /* if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
@@ -135,7 +135,7 @@ public class MainActivity extends AppCompatActivity
             finish();
             Intent intent=new Intent(this,LoginActivity.class);
             startActivity(intent);
-        }
+        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
