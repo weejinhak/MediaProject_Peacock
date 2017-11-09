06aba5b2c46a5d863b989bf815b0346b6a3a9ffb
Jinhak Wee <wlsgkr91@gmail.com>
2017-11-09 14:27:39 +0900

    진학액션바 툴바로 바꿈

--- app/src/main/java/com/peac/cock/peacock_project/LoginActivity.java
+++ app/src/main/java/com/peac/cock/peacock_project/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.peac.cock.peacock_project;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -46,6 +48,11 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //툴바 설정
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
 
         //firebase Auth정보 get
         mAuth = FirebaseAuth.getInstance();
