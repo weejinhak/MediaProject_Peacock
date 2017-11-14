9f600422c341cc7dc0de1e2a9a0c5a7996cf637f
dahye <dahye96@gmail.com>
2017-11-14 18:22:05 +0900

    계산 페이지 뒤로 가기 버튼 이벤트 추가

--- app/src/main/java/com/peac/cock/peacock_project/CalculatorActivity.java
+++ app/src/main/java/com/peac/cock/peacock_project/CalculatorActivity.java
@@ -1,9 +1,12 @@
 package com.peac.cock.peacock_project;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
@@ -21,6 +24,7 @@ public class CalculatorActivity extends AppCompatActivity {
         final ImageButton outgoingButton = findViewById(R.id.calculator_layout_outgoing_button);
         final ImageButton transferButton = findViewById(R.id.calculator_layout_transfer_button);
         final ImageButton checkButton = findViewById(R.id.calculator_layout_check_button);
+        final ImageButton backButton = findViewById(R.id.calculator_layout_back_button);
 
 
         outgoingButton.setOnTouchListener(new View.OnTouchListener() {
@@ -67,6 +71,37 @@ public class CalculatorActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), HandwritingActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder ad = new AlertDialog.Builder(CalculatorActivity.this);
+
+                ad.setTitle("잠깐만");       // 제목 설정
+                ad.setMessage("페이지를 나가면 작성된 내용이 사라집니다. 나가시겠습니까?");   // 내용 설정
+
+                // 확인 버튼 설정
+                ad.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();     //닫기
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
+                // 취소 버튼 설정
+                ad.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();     //닫기
+                    }
+                });
+                // 창 띄우기
+                ad.show();
             }
         });
     }
