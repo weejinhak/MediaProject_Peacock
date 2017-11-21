cae967b47b0ef60a80f8173ac4d3bc5b9ff806af
LeeEunk <dmsrud7817@naver.com>
2017-11-22 01:04:35 +0900

    calendar & list 수정중_ing

--- app/src/main/java/com/peac/cock/peacock_project/Tab1_content.java
+++ app/src/main/java/com/peac/cock/peacock_project/Tab1_content.java
@@ -3,12 +3,19 @@ package com.peac.cock.peacock_project;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +27,11 @@ import java.util.List;
 public class Tab1_content extends Fragment implements AdapterView.OnItemClickListener{
 
 
+    /*private ImageButton mPreviousButton, mForwardButton;
+    private TextView mCurrentMonthLabel;
+    private int mCurrentPage;
+    private ViewPager mViewPager;*/
+
     String[] placeName;
     TypedArray categoryPicId;
     String[] purchase;
@@ -28,19 +40,21 @@ public class Tab1_content extends Fragment implements AdapterView.OnItemClickLis
     List<ListViewRowItem> rowItems;
     ListView mylistview;
 
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private String uid;
+
+    //private ImageView categoryImage;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.activity_listview, container, false);
 
+        View rootView = inflater.inflate(R.layout.activity_tab_list, container, false);
         rowItems = new ArrayList<ListViewRowItem>();
-
         placeName = getResources().getStringArray(R.array.place_name);
-
         categoryPicId = getResources().obtainTypedArray(R.array.category_pic_id);
-
         purchase = getResources().getStringArray(R.array.purchase);
-
         accountType = getResources().getStringArray(R.array.account_type);
 
         for (int i = 0; i < placeName.length; i++) {
@@ -54,13 +68,22 @@ public class Tab1_content extends Fragment implements AdapterView.OnItemClickLis
         mylistview.setAdapter(adapter);
 
         mylistview.setOnItemClickListener(this);
+
+        //fireBase Auth & Database
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        uid=mAuth.getCurrentUser().getUid();
+
+       // categoryImage = rootView.findViewById(R.id.category_add_layout_incoming_button);
+
+
         return rootView;
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String member_name = rowItems.get(position).getMember_name();
-        Toast.makeText(getContext(), "" + member_name, Toast.LENGTH_SHORT).show();
+        String placeName = rowItems.get(position).getPlaceName();
+        Toast.makeText(getContext(), "" + placeName, Toast.LENGTH_SHORT).show();
     }
 
 }
