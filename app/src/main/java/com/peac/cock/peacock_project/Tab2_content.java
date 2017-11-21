4dc5b965d7e43204c6ef70e4335e38217088d3f2
LeeEunk <dmsrud7817@naver.com>
2017-11-21 22:06:58 +0900

    calendar & list 수정중

--- app/src/main/java/com/peac/cock/peacock_project/Tab2_content.java
+++ app/src/main/java/com/peac/cock/peacock_project/Tab2_content.java
@@ -17,52 +17,12 @@ import java.util.List;
  * Created by dmsru on 2017-11-13.
  */
 
-public class Tab2_content extends Fragment implements AdapterView.OnItemClickListener {
-
-    String[] placeName;
-    TypedArray categoryPicId;
-    String[] purchase;
-    String[] accountType;
-
-    List<ListViewRowItem> rowItems;
-    ListView mylistview;
+public class Tab2_content extends Fragment  {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.activity_listview, container, false);
-
-        rowItems = new ArrayList<ListViewRowItem>();
-
-        placeName = getResources().getStringArray(R.array.place_name);
-
-        categoryPicId = getResources().obtainTypedArray(R.array.category_pic_id);
-
-        purchase = getResources().getStringArray(R.array.purchase);
-
-        accountType = getResources().getStringArray(R.array.account_type);
-
-        for (int i = 0; i < placeName.length; i++) {
-            ListViewRowItem item = new ListViewRowItem(placeName[i],
-                    categoryPicId.getResourceId(i, -1), purchase[i], accountType[i]);
-            rowItems.add(item);
-        }
-
-        mylistview = (ListView) rootView.findViewById(R.id.List);
-        ListViewCustomAdapter adapter = new ListViewCustomAdapter(getContext(), rowItems);
-        mylistview.setAdapter(adapter);
-
-        mylistview.setOnItemClickListener(this);
-
-
-
-
+        View rootView = inflater.inflate(R.layout.activity_tab_calendar, container, false);
         return rootView;
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String member_name = rowItems.get(position).getMember_name();
-        Toast.makeText(getContext(), "" + member_name, Toast.LENGTH_SHORT).show();
     }
 }
