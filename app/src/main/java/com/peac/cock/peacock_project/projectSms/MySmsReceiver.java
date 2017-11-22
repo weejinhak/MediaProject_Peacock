d965329c288b42cd4cfd38445e2bad182a45718f
Jinhak Wee <wlsgkr91@gmail.com>
2017-11-23 02:15:05 +0900

    문자받은 후 DB저장 완료

--- app/src/main/java/com/peac/cock/peacock_project/projectSms/MySmsReceiver.java
+++ app/src/main/java/com/peac/cock/peacock_project/projectSms/MySmsReceiver.java
@@ -8,11 +8,22 @@ import android.telephony.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.peac.cock.peacock_project.projectDto.Asset;
+import com.peac.cock.peacock_project.projectDto.LedgerDto;
+
 /**
  * Created by wee on 2017. 11. 21..
  */
 
 public class MySmsReceiver extends BroadcastReceiver {
+
+    private LedgerDto ledgerDto;
+    private FirebaseAuth auth;
+    private FirebaseDatabase mDatabase;
+    private String uid;
+
     public MySmsReceiver() {
 
     }
@@ -23,6 +34,12 @@ public class MySmsReceiver extends BroadcastReceiver {
         String str = ""; // 출력할 문자열 저장
         String number = "";
         String body="";
+
+        ledgerDto= new LedgerDto();
+        auth=FirebaseAuth.getInstance();
+        mDatabase=FirebaseDatabase.getInstance();
+        uid=auth.getCurrentUser().getUid();
+
         if (bundle != null) { // 수신된 내용이 있으면
             // 실제 메세지는 Object타입의 배열에 PDU 형식으로 저장됨
 
@@ -39,6 +56,40 @@ public class MySmsReceiver extends BroadcastReceiver {
             }
             //System.out.println(str);
             System.out.println("문자전화번호:" + number);
+
+            //message 파싱후 DB저장
+            if(number.equals("01032368187")){
+                String message=body;
+                String[] messageToken = message.split("\n");
+                String smsCardName = "";
+                String smsDate = "";
+                String smsTime = "";
+                String smsPrice = "";
+                String smsPlace = "";
+
+                if (messageToken.length == 6) {
+
+                    smsCardName = messageToken[1];
+
+                    String date = messageToken[3];
+                    String[] dateToken = date.split("\\s");
+                    smsDate = dateToken[0];
+                    smsTime = dateToken[1];
+                    String price = messageToken[4];
+                    String[] priceToken = price.split("원");
+                    smsPrice = priceToken[0].replaceAll("\\,", "");
+                    smsPlace = messageToken[5];
+
+                    ledgerDto.setInOut("지출");
+                    ledgerDto.setDate(smsDate);
+                    ledgerDto.setTime(smsTime);
+                    ledgerDto.setAmount(smsPrice);
+                    ledgerDto.setContent(smsPlace);
+                    ledgerDto.setAsset(new Asset(smsCardName));
+                    mDatabase.getReference().child("users").child(uid).child("ledger").push().setValue(ledgerDto);
+                }
+
+            }
             Log.d("Tag",body);
             System.out.println("문자내용"+"\n"+body);
         }
